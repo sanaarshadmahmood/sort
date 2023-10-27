773172f31e389db22cf9843b04e04d7429fe8d92
sanaarshadmahmood <148863573+sanaarshadmahmood@users.noreply.github.com>
2023-10-27 16:21:41 +0100

    Add files via upload

---
+++ untitled12.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 26 20:44:22 2023
+
+@author: SANA
+"""
+
+import numpy as np
+x = np.array([2,1,4,3,5])
+y=np.sort(x)
+print(y)
+i = np.argsort(x)
+print(x)
